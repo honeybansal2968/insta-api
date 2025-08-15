8fcb6c913b085d864c2117edf2ead9456ff41ff2
honeybansal2968 <honeybansal2968@gmail.com>
2025-08-15 13:56:47 +0530

    first comit

--- api/index.py
+++ api/index.py
@@ -1,11 +1,35 @@
+import json
+
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World! </p>"
 
-@app.route('/about')
-def about():
-    return 'About'
+@app.route("/privacy_policy")
+def privacy_policy():
+    with open("./privacy_policy.html", "rb") as file:
+        privacy_policy_html = file.read()
+    return privacy_policy_html
+
+@app.route("/webhook", methods = ["GET", "POST" ])
+def webhook():
+    if request.method == "POST":
+        try:
+            pass
+        except :
+            pass
+        return "<p>This is POST Request, Hello Webhook !< /p>"
+    if request.method == "GET":
+        hub_mode = request.args.get("hub.mode")
+        hub_challenge = request.args.get("hub.challenge")
+        hub_verify_token = request.args.get("hub.verify_token")
+        if hub_challenge:
+            return hub_challenge
+        else:
+            return "<p>This is GET Request, Hello Webhook !< /p>"
+
+print(json.dumps(request.get_json(), indent=4))
